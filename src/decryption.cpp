506b8428f65bfc919c0326ce3c317c6e3828ed99
AddiThurston <addithurston@gmail.com>
2026-01-20 12:32:35 -0600

    program counts frequency of each ciphertext character

--- src/decryption.cpp
+++ src/decryption.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_set>
+#include <map>
 #include <string>
 
 using namespace std;
 
+// reads a file to an unordered_set of strings
 int readToSet(unordered_set<string>& d, string fileName) {
     ifstream fin(fileName);
     if (!fin.is_open()) {
@@ -20,35 +22,55 @@ int readToSet(unordered_set<string>& d, string fileName) {
     return 0;
 }
 
+// reads a file to a single string
 int readToString(string& s, string fileName) {
+    s = "";
     ifstream fin(fileName);
-    if (!fin.is_open()) {
+    if (!fin.is_open()) {   // check that the file is opened correctly
         cout << "Error opening " << fileName << endl;
         return -1;
     }
 
-    string line;
-    while (fin >> line) {
-        s += line;
+    string input;
+    while (fin >> input) {
+        s += input;  // keep appending the string with the new input
     }
     fin.close();
     return 0;
 }
 
+// reads the frequency of letters to a map
+void getCharFreq(string& s, map<char, int>& freq) {
+    // initialize the frequency of the characters to 0
+    for (char c = 'A'; c <= 'Z'; c++) {
+        freq.insert({c, 0});
+    }
+
+    for (char& c : s) {
+        freq[c]++; // increment the frequency of the current character by 1;
+    }
+}
+
 int main() {
-    unordered_set<string> dict;
-    string ciphertext = "";
+    unordered_set<string> dict; // the dictionary from dictionary.txt
+    string ciphertext;  // the ciphertext from ciphertext.txt
+    map<char, int> cipherFreq; // the frequency of all letters in the ciphertext
 
     if (readToSet(dict, "dictionary.txt") == -1) {
         return 1;
     }
     cout << "Done reading dictionary file\n";
-    cout << dict.size() << endl;
 
     if (readToString(ciphertext, "ciphertext.txt") == -1) {
         return 1;
     }
     cout << "Done reading ciphertext file\n";
-    cout << ciphertext << endl;
+
+    getCharFreq(ciphertext, cipherFreq);
+    cout << "Done counting frequencies of ciphertext letters\n";
+    for (const auto& pair : cipherFreq) {
+        std::cout << pair.first << " - " << pair.second << std::endl;
+    }
+    
     return 0;
 }
